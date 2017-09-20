0b90743f23ebbef7d2800f1b2298bc314d458150
Brian Ethier <davidbrianethier@gmail.com>
2017-09-19 21:42:09 -0600

    Adding jquery etc

---
+++ _includes/main.js
@@ -0,0 +1,11 @@
+function setHeight() {
+  // var height = $(window).css("height");
+  var height = document.body.clientHeight;
+  // console.log(height)
+  // thirtypc = parseInt(height) + 'px';
+  $(".section").css('height', height);
+}
+
+$(document).ready(function() {
+  $(window).bind('resize', setHeight);
+});
